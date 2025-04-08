deedfa44507b2b5278e7952d20e159f020d7fee3
sushil thakur <sushilt2059@gmail.com>
2025-04-08 20:14:01 +0545

    psu

--- backend/db.connection.js
+++ backend/db.connection.js
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config(); // Load environment variables
 
 const dbConnect = async () => {
   try {
-    const url =
-    "mongodb+srv://realestate:1234@sushil.rf9fj.mongodb.net/?retryWrites=true&w=majority&appName=sushil"
+    const url = process.env.MONGODB_URI;
 
-    await mongoose.connect(url);
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
     console.log("DB connection successful...");
   } catch (error) {
